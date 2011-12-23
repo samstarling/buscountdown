d853fa33eefae7902aff798035373ffbe1a88ff7
Sam Starling <sam.starling@gmail.com>
2011-12-23 18:01:19 +0000

    Route-specific view fixed

--- application.rb
+++ application.rb
@@ -41,8 +41,26 @@ get '/stop/:id' do
   erb :stop_detail
 end
 
+get '/stop/:id/favourite' do
+  cookie = request.cookies["bus_stops"]
+  if cookie
+    stops = cookie.split(',')
+    if stops.include? params[:id]
+      stops.delete(params[:id])
+    else
+      stops << params[:id]
+    end
+    response.set_cookie("bus_stops", {:value => stops.join(','), :path => "/"})
+  else
+    response.set_cookie("bus_stops", {:value => params[:id], :path => "/"})
+  end
+  redirect request.referer
+end
+
+
 get '/stop/:id/:route' do
   @stop = params[:id]
+  @route = params[:route]
   response = open(API_URL % @stop){ |f| f.read }
   @data = JSON.parse(response)
   @buses = Array.new
@@ -59,24 +77,10 @@ get '/stop/:id/:route' do
       @is_favourite = true
     end
   end
-  erb :stop_detail
+  erb :stop_route_detail
 end
 
-get '/stop/:id/favourite' do
-  cookie = request.cookies["bus_stops"]
-  if cookie
-    stops = cookie.split(',')
-    if stops.include? params[:id]
-      stops.delete(params[:id])
-    else
-      stops << params[:id]
-    end
-    response.set_cookie("bus_stops", {:value => stops.join(','), :path => "/"})
-  else
-    response.set_cookie("bus_stops", {:value => params[:id], :path => "/"})
-  end
-  redirect request.referer
-end
+
 
 post '/search' do
   redirect "/stop/#{params[:stop]}"
